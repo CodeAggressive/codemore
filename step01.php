e660af6b77811767ff50b2f18bc564be7ccc4318
codelighter <shb8845369@gmail.com>
2017-04-28 11:33:43 +0800

    dev合并后的文件

--- step01.php
+++ step01.php
@@ -1,10 +1,7 @@
 <?php
 	echo "I am master step 01";
-<<<<<<< HEAD
 	echo "I am dev step 02";
-=======
 	echo "I am dev step 01";
 	echo "I am master step 02";
 	echo "I am master step 03";
->>>>>>> master
 ?>
